543be09cf85fe2e936b089c9a120459284e799d1
Anton Sauchyk <antonsauchyk@gmail.com>
2026-01-20 11:05:45 +0100

    fix: use explicit re-exports in routers init

--- backend/server/routers/__init__.py
+++ backend/server/routers/__init__.py
@@ -1,3 +1,10 @@
 """API routers."""
 
-from server.routers import data, pipeline, prices, portfolio_prices, wallet, trading
+from server.routers import (
+    data as data,
+    pipeline as pipeline,
+    portfolio_prices as portfolio_prices,
+    prices as prices,
+    trading as trading,
+    wallet as wallet,
+)
